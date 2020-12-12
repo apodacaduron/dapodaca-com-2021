06592906f2dbc0bffcbc53b9b87c922f98258e18
Daniel Apodaca <tanjiro@Daniels-MacBook.local>
2020-12-12 10:30:04 -0700

    fix(fade): fade heading width fix

--- src/views/Heading/Heading.tsx
+++ src/views/Heading/Heading.tsx
@@ -6,13 +6,13 @@ import './Heading.sass';
 const Heading = () => {
   return (
     <div id='heading'>
-      <div className='title-container'>
-        <Fade cascade>
+      <Fade cascade>
+        <div className='title-container'>
           <h1 className='title'>Daniel</h1>
           <h1 className='title'>Apodaca</h1>
           <div className='subtitle'>I’m a Fullstack Developer & Designer</div>
-        </Fade>
-      </div>
+        </div>
+      </Fade>
       <div className='circle'></div>
     </div>
   );
